af645877ccc47c454d30bff3531a4a99c42bdeaa
the-bogeyman <b45b26@gmail.com>
2015-01-25 16:01:48 +0600

    Edit some file in CodeBlocks project - Class Routine.

--- C_programming/CodeBlocks/Class Routine/routine.c
+++ C_programming/CodeBlocks/Class Routine/routine.c
@@ -1,3 +1,17 @@
+/******************************************************************************
+* TTTTTTTTT H    H EEEEE    BBB    O   GGGG  EEEEE MM   MM     A     NN    N  *
+*     T     H    H E        B  B  O O  G     E     M M M M    A A    N N   N  *
+*     T     HHHHHH EEE  --- BBB  O   O G GGG EEE   M  M  M   AAAAA   N  N  N  *
+*     T     H    H E        B  B  O O  G  GG E     M     M  A     A  N   N N  *
+*     T     H    H EEEEE    BBB    O    GG G EEEEE M     M A       A N    NN  *
+*                                                                             *
+******************************************************************************/
+
+/*
+This source code is available for anyone to edit, rewrite or take any part of code for another program or project.
+But please give me credit for my part of work in your project.
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
